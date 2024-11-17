c9b6890f13881fb884e2fc310ee5e20b48f15600
Rashmi Patil <81952851+Rashmi-Patil08@users.noreply.github.com>
2024-11-17 16:31:40 +1100

    Update firebase cloud function and message hllo from firebase cloud function

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,12 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
+const cors = require("cors")({origin: true});
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Example: A simple HTTP function that returns a greeting message
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    response.send("Hello from Firebase Cloud Functions!");
+  });
+});
